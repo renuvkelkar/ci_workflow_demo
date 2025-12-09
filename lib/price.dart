e8f22b4ff8b6cfc432724503a2413d909ef6fd78
Renuka Kelkar <32317593+renuvkelkar@users.noreply.github.com>
2025-12-09 16:27:23 +0000

    changed the price dart file

--- lib/price.dart
+++ lib/price.dart
@@ -1,9 +1,6 @@
-// int calculateTotal(int price, int quantity, {double discount = 0.0}) {
-//   final subtotal = price * quantity;
-//   final discounted = subtotal * discount;
-//   return subtotal - discounted.toInt();
-// }
 int calculateTotal(int price, int quantity, {double discount = 0.0}) {
-  // WRONG ON PURPOSE to see CI failing
-  return 999;
+  final subtotal = price * quantity;
+  final discounted = subtotal * discount;
+  return subtotal - discounted.toInt();
 }
+
